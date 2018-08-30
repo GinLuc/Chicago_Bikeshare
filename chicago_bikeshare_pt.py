737d0e6d615b3f7541fbe0afbdb8c0424ffbc6e2
GianL <gianluccaseverino@gmail.com>
2018-08-29 23:08:03 -0300

    master

--- chicago_bikeshare_pt.py
+++ chicago_bikeshare_pt.py
@@ -60,6 +60,7 @@ input("Aperte Enter para continuar...")
 def column_to_list(data, index):
     column_list = []
     # Dica: Você pode usar um for para iterar sobre as amostras, pegar a feature pelo seu índice, e dar append para uma lista
+    column_list.append(index)
     for i in data:
         column_list.append(i[index])
             
